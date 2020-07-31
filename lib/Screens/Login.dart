b678ea9e636d8a3271ce295c0f0f69582b3d7683
NayanVerma10 <NayanVerma10@users.noreply.github.com>
2020-07-31 22:58:05 +0530

    save karna bhul gaya tha

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -13,69 +13,90 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
-  LoginWithEmail(){
-
+  LoginWithEmail() {
+    // TODO
   }
 
-  LoginWithGoogle(){
-    
+  LoginWithGoogle() {
+    // TODO
   }
-  LoginWithFacebook(){
-      
-    }
+  LoginWithFacebook() {
+    // TODO
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(child: Text('Our App'),),
+      drawer: Drawer(
+        child: Text('Our App'),
+      ),
       appBar: AppBar(
         title: Text('This is our Our'),
-        
       ),
       body: Container(
         child: Center(
-          child: Column(
-            
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 300,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    icon: Icon(Icons.email,color: Colors.purple,),
-                    prefixStyle: TextStyle(color:Colors.purple),
-                    hintStyle: TextStyle(color:Colors.purple),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Container(
+              width: 300,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.purple,
                   ),
-
+                  prefixStyle: TextStyle(color: Colors.purple),
+                  hintStyle: TextStyle(color: Colors.purple),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(bottom:20),
-                width: 300,
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    icon: Icon(FontAwesomeIcons.lock,color: Colors.purple,),
-                    prefixStyle: TextStyle(color:Colors.purple),
-                    hintStyle: TextStyle(color:Colors.purple),
-
-                    hintText: 'Password',
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 20),
+              width: 300,
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    FontAwesomeIcons.lock,
+                    color: Colors.purple,
                   ),
-
+                  prefixStyle: TextStyle(color: Colors.purple),
+                  hintStyle: TextStyle(color: Colors.purple),
+                  hintText: 'Password',
                 ),
               ),
-              RaisedButton(onPressed: (){LoginWithEmail();},child: Text('Login'),),
-              SizedBox(height: 100,),
-              Container(width: 300,child: RaisedButton.icon(padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),onPressed: (){LoginWithGoogle();}, icon: Icon(FontAwesomeIcons.google), label: Text('Sign in With Google'))),
-              Container(width: 300,child: RaisedButton.icon(padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),onPressed: (){LoginWithFacebook();}, icon: Icon(FontAwesomeIcons.facebook), label: Text('Sign in With Facebook'))),
-              
-            ]
-          ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                LoginWithEmail();
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+                width: 300,
+                child: RaisedButton.icon(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    onPressed: () {
+                      LoginWithGoogle();
+                    },
+                    icon: Icon(FontAwesomeIcons.google),
+                    label: Text('Sign in With Google'))),
+            Container(
+                width: 300,
+                child: RaisedButton.icon(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    onPressed: () {
+                      LoginWithFacebook();
+                    },
+                    icon: Icon(FontAwesomeIcons.facebook),
+                    label: Text('Sign in With Facebook'))),
+          ]),
         ),
       ),
-      
     );
   }
 }
